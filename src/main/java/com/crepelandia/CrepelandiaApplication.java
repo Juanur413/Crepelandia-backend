b22f4aba28b1e28f075cb9d291656faa9eaad049
Juan Pablo Uribe Romero <110949043+Juanur413@users.noreply.github.com>
2025-06-10 23:02:50 -0500

    fix: mover CORS dentro de clase principal

--- src/main/java/com/crepelandia/CrepelandiaApplication.java
+++ src/main/java/com/crepelandia/CrepelandiaApplication.java
@@ -2,10 +2,27 @@ package com.crepelandia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrepelandiaApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CrepelandiaApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://meek-sfogliatella-bf1d24d.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
